73a5b9c6fa42c4b79d2a8294c55ef2743559c350
Nguyen Tien Hoang <nguyentienhoang139@gmail.com>
2017-04-04 10:31:55 +0700

    finish

--- PhieuKiemKe/PhieuKiemKe/Program.cs
+++ PhieuKiemKe/PhieuKiemKe/Program.cs
@@ -42,8 +42,7 @@ namespace PhieuKiemKe
                 a.lb_date.Text = "Ngày: ";
                 a.lb_time.Text = "Giờ: ";
                 a.lb_batchno.Text = "BatchName: ";
-                a.btn_thoat.Text = "Thoát";
-                a.chb_hienthi.Text = "Hiển Thị";
+                a.btn_thoat.Text = "Thoát";a.chb_hienthi.Text = "Hiển Thị";
                 a.chb_luu.Text = "Lưu";
                 a.lb_version.Text = @"1.0.7";
                 a.UrlUpdateVersion = @"\\10.10.10.254\DE_Viet\2017\PHIEU_KIEM_KE\TOOL\";
